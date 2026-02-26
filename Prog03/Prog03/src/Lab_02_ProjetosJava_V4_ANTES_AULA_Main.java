600de84c90d26b1f10a2af689459e19191b03509
Gabriel Fernandes Riguetto <gabrielriguetto3@gmail.com>
2026-02-26 11:07:23 -0300

    Add files via upload

---
+++ Prog03/Prog03/src/Lab_02_ProjetosJava_V4_ANTES_AULA_Main.java
@@ -0,0 +1,39 @@
+// ------------------------------------
+// Disc.: IED-001 - ESTRUTURAS DE DADOS
+// Prof.: Carlos - FATEC S J Rio Preto
+// Lab. : PROJETOS JAVA
+// Ano/S: 2026/1S
+// ------------------------------------
+
+// VERSÃO 4 (antes da aula): idem final da versão 3
+// - dois arquivos separados
+// - ações realizadas por métodos separados do 'main'
+// --------------------------------------------------
+
+// importação de classe para leitura de dados do teclado -> NÃO PRECISA MAIS
+//import java.util.Scanner;
+
+// classe que contém o PROGRAMA PRINCIPAL ('main')
+public class Lab_02_ProjetosJava_V4_ANTES_AULA_Main {
+
+	// PROGRAMA PRINCIPAL
+	// ------------------
+	public static void main(String[] args) throws Exception {
+		Lab_02_ProjetosJava_V4_ANTES_AULA_Coordenador coord = new Lab_02_ProjetosJava_V4_ANTES_AULA_Coordenador(); //instanciaçãpo do coordenador
+		coord.executarTudo();
+
+	}
+
+}
+
+/*  // identificação do autor do trabalho
+		System.out.println();
+		System.out.println("------------------------------------");
+		System.out.println("Disc.: ESTRUTURAS DE DADOS - 2026/1S");
+		System.out.println("Lab. : FUNDAMENTOS DE JAVA");
+		System.out.println("------------------------------------");
+		System.out.println("Nome : GABRIEL FERNANDES RIGUETTO");
+		System.out.println("R.A. : 121 092 251 1025");
+		System.out.println("------------------------------------");
+		System.out.println();
+*/
